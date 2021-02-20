737079ffec405b2ed878c1008a5f091b55007e0e
Iago Barbosa <iago.barbosa@saeb.ba.gov.br>
2021-02-20 11:35:49 -0300

    primeiro commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+
+Route::get('/', ['as' => 'site.home', 'uses' => 'Site\LoginController@index']);
+
+Route::get('/login', ['as' => 'site.login', 'uses' => 'Site\LoginController@index']);
+
+Route::get('/login/sair', ['as' => 'site.login.sair', 'uses' => 'Site\LoginController@sair']);
+
+Route::post('/login/entrar', ['as' => 'site.login.entrar', 'uses' => 'Site\LoginController@entrar']);
+
+
+
+Route::group(['middleware' => 'auth'], function () {
+
+  Route::get('/admin/cursos', ['as' => 'admin.cursos', 'uses' => 'BaseController@index']);
+
+ 
+});
